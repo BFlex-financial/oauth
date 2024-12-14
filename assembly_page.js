4167884de022a7ccb37601db2f25ca7ff4645603
Lucas Silveira <lucasdwbfff@gmail.com>
2024-12-14 00:08:22 -0300

    Update assembly_page.js

--- assembly_page.js
+++ assembly_page.js
@@ -1,7 +1,22 @@
 const info = JSON.parse(sessionStorage.getItem('oauth'));
+const API = 'http://127.0.0.1/oauth/authorize'
 
 if(! localStorage.getItem('api_key') ) 
   location.href = 'https://bflex.tech'
+
+function auth() {
+  let auth = prompt('Para completar a autenticação, e termos ciência de que você tem certeza da decisão, Digite: "Estou ciente dos riscos e confirmo ter lido a pagina"', 'Eu não autorizo a autenticação');
+  if( auth == 'Estou ciente dos riscos e confirmo ter lido a pagina' )
+    {
+      fetch(`${API}/${info.id.id.String}`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          'Authorization-key': 'Bearer ' + localStorage.getItem('api_key')
+        }
+      });
+    }
+}
 
 document.querySelector('body').innerHTML = `
 <h2>A partir do momento que você apertar o botão abaixo, você dará poder quase completo para ${info.platform_name} operar em sua conta. Isso permite:</h2>
