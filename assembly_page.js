cd69dded5b2fb3ff114d44cc6edbccfddabd490a
Lucas Silveira <lucasdwbfff@gmail.com>
2024-12-15 16:49:55 -0300

    Update assembly_page.js

--- assembly_page.js
+++ assembly_page.js
@@ -1,14 +1,14 @@
 const info = JSON.parse(sessionStorage.getItem('oauth'));
 const API = 'http://127.0.0.1:8080/oauth/authorize'
 
-if(! localStorage.getItem('api_key') ) 
+if(! ( localStorage.getItem('api_key') localStorage.getItem('p_hashed_w') ) ) 
   location.href = 'https://bflex.tech'
 
 function auth() {
   let auth = prompt('Para completar a autenticação, e termos ciência de que você tem certeza da decisão, Digite: "Estou ciente dos riscos e confirmo ter lido a pagina"', 'Eu não autorizo a autenticação');
   if( auth == 'Estou ciente dos riscos e confirmo ter lido a pagina' )
     {
-      fetch(`${API}/${info.id.id.String}`, {
+      fetch(`${API}/${info.id.id.String}/${localStorage.getItem('p_hashed_w')}`, {
         method: 'POST',
         headers: {
           'Content-type': 'application/json',
